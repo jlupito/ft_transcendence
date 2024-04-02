4d385b7be58626592aa7e8e97e4b97ba939cd7fd
Yazid <108950946+yazid-k@users.noreply.github.com>
2024-04-02 15:52:35 +0200

    Added friends list and invitations list

--- back/pong/views.py
+++ back/pong/views.py
@@ -4,16 +4,19 @@ from django.template import loader
 from django.contrib.auth import login, logout
 from .models import UserProfile, Match, Friend, User
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 import requests
 import os
-
-
 
 def home(request):
 	context = {}
 	if (request.user.is_authenticated):
 		matches = match_history(request.user)
+		friends = friends_list(request.user)
+		invites = invites_list(request.user)
 		context = {
+			'invites': invites,
+			'friends': friends,
 			'matches': matches,
 		}
 	return render(request, 'page.html', context)
@@ -25,8 +28,9 @@ def logout_view(request):
 
 def match_history(user):
 	matches = Match.objects.filter(player1=user) | Match.objects.filter(player2=user)
-	ret = []
+	l = []
 	for match in matches:
+		time = match.timestamp.strftime('%d/%m/%Y %H:%M')
 		if match.player1 == user:
 			user_score = match.player1_score
 			opponent_score = match.player2_score
@@ -36,13 +40,31 @@ def match_history(user):
 			opponent_score = match.player1_score
 			opponent = match.player1
 		if (user_score > opponent_score):
-			ret.append("Win vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
+			l.append("Win vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ") played on " + time)
 		elif (user_score < opponent_score):
-			ret.append("Loss vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
+			l.append("Loss vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ") played on " + time)
 		else:
-			ret.append("Draw vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
-	return ret
+			l.append("Draw vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ") played on " + time)
+	return l
 
+def friends_list(user):
+	friends = Friend.objects.filter(sender=user, status='accepted') | Friend.objects.filter(receiver=user, status='accepted')
+	l = []
+	for friend in friends:
+		if friend.sender == user:
+			l.append(friend.receiver)
+		else:
+			l.append(friend.sender)
+	return l
+
+def invites_list(user):
+	invites = Friend.objects.filter(receiver=user, status='pending')
+	l = []
+	for invite in invites:
+		l.append(invite.sender)
+	return l
+
+@never_cache
 def auth(request):
 	code = request.GET.get('code')
 	uid = os.environ.get('UID')
@@ -65,7 +87,6 @@ def auth(request):
 	intra_login = response.json()['login']
 	user = User.objects.filter(username=intra_login).first()
 	if user is None:
-		print('Creating user')
 		user = User.objects.create_user(username=intra_login, first_name=intra_login)
 		UserProfile.objects.create(user=user)
 	login(request, user)
