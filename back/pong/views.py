bbf7694c180f432852dc78aafa362df759c08cd2
yazid-k <yazidkadiri@outlook.com>
2024-04-02 14:27:02 +0200

    Added match history

--- back/pong/views.py
+++ back/pong/views.py
@@ -1,17 +1,47 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from .models import UserProfile, Match, Friend, User
+from django.contrib.auth.decorators import login_required
 import requests
 import os
 
-def home(request):
-	return render(request, 'page.html')
 
+
+def home(request):
+	context = {}
+	if (request.user.is_authenticated):
+		matches = match_history(request.user)
+		context = {
+			'matches': matches,
+		}
+	return render(request, 'page.html', context)
+
+@login_required
 def logout_view(request):
 	logout(request)
 	return redirect('home')
+
+def match_history(user):
+	matches = Match.objects.filter(player1=user) | Match.objects.filter(player2=user)
+	ret = []
+	for match in matches:
+		if match.player1 == user:
+			user_score = match.player1_score
+			opponent_score = match.player2_score
+			opponent = match.player2
+		else:
+			user_score = match.player2_score
+			opponent_score = match.player1_score
+			opponent = match.player1
+		if (user_score > opponent_score):
+			ret.append("Win vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
+		elif (user_score < opponent_score):
+			ret.append("Loss vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
+		else:
+			ret.append("Draw vs " + opponent.username + " (" + str(user_score) + " - " + str(opponent_score) + ")")
+	return ret
 
 def auth(request):
 	code = request.GET.get('code')
