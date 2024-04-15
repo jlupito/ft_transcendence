d4eb38519b62f23872acb57a5a19c58684f34784
jlupito <jl.arthaud@protonmail.com>
2024-04-15 18:09:33 +0200

    login page

---
+++ back/pong/static/scripts/script.js
@@ -0,0 +1,60 @@
+const ball = document.getElementById('ball');
+let angle = 90;
+let speedX = 2;
+let speedY = 2;
+let posX = window.innerWidth / 2;
+let posY = window.innerWidth / 2;
+
+function updateBackground() {
+    document.body.style.background = `linear-gradient(${angle}deg, #ff006a, #00ffdd)`;
+  }
+
+function launchBall() {
+    posX += speedX;
+    posY += speedY;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const ballWidth = ball.offsetWidth;
+    const ballHeight = ball.offsetHeight;
+
+    if (posX <= 0) {
+        speedX *= -1;
+        angle = 90;
+      }
+
+    if (posX + ballWidth >= windowWidth) {
+        speedX *= -1;
+        angle = 270;
+      }
+
+    if (posY <= 0) {
+        speedY *= -1;
+        angle = 180;
+      }
+      
+    if (posY + ballHeight >= windowHeight) {
+        speedY *= -1;
+        angle = 0;
+    }
+    
+    updateBackground(angle);
+    ball.style.left = posX + 'px';
+    ball.style.top = posY + 'px';
+    // console.log(posX, posY);
+    // console.log(windowWidth, windowHeight);
+
+}
+
+setInterval(launchBall, 3);
+
+const language = document.querySelectorAll('.chooseLanguage');
+language.forEach(item => {
+    item.addEventListener('click', () => {
+        language.forEach(item => {
+            item.classList.remove('active');
+        });
+
+        item.classList.add('active');
+    });
+});
