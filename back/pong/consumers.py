0b0d387040cdc2a4236135084defe5e78adc6b13
sachayakovleff <sacha.yakovleff@outlook.fr>
2024-05-20 16:29:14 +0200

    simplified code

--- back/pong/consumers.py
+++ back/pong/consumers.py
@@ -8,40 +8,34 @@ import threading
 import time
 from .models import Match
 
+
 games_online = []
 games_local = []
 
 class Game():
-    def __init__(self, maxscore):
+    def __init__(self, maxscore, game_type):
+        self.game_type = game_type
         self.delay = 30
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
         self.player1 = ""
         self.player2 = ""
-
         self.WIDTH = 600
         self.HEIGHT = 600
-
         self.paddle_speed = 5
         self.paddle_width = 10
         self.paddle_height = 100
-
         self.p1_x_pos = 10
         self.p1_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
-
-
         self.p2_x_pos = self.WIDTH - self.paddle_width - 10
         self.p2_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
-
         self.p1_score = 0
         self.p2_score = 0
         self.maxscore = maxscore
-
         self.p1_up = False
         self.p1_down = False
         self.p2_up = False
         self.p2_down = False
-
         self.ball_x_pos = self.WIDTH / 2
         self.ball_y_pos = self.HEIGHT / 2
         self.ball_width = 8
@@ -51,51 +45,49 @@ class Game():
         self.has_finished = False
 
     def apply_player_movement(self):
-        if (self.p1_up):
+        if self.p1_up:
             self.p1_y_pos = max(self.p1_y_pos - self.paddle_speed, 0)
-        elif (self.p1_down):
+        elif self.p1_down:
             self.p1_y_pos = min(self.p1_y_pos + self.paddle_speed, self.HEIGHT - self.paddle_height)
-        if (self.p2_up):
+        if self.p2_up:
             self.p2_y_pos = max(self.p2_y_pos - self.paddle_speed, 0)
-        elif (self.p2_down):
+        elif self.p2_down:
             self.p2_y_pos = min(self.p2_y_pos + self.paddle_speed, self.HEIGHT - self.paddle_height)
 
     def apply_ball_movement(self):
         if (self.ball_x_pos + self.ball_x_velocity < self.p1_x_pos + self.paddle_width
-            and (self.p1_y_pos < self.ball_y_pos + self.ball_y_velocity + self.ball_width and self.ball_y_pos + self.ball_y_velocity + self.ball_width < self.p1_y_pos + self.paddle_height + 10)):
+            and (self.p1_y_pos < self.ball_y_pos + self.ball_y_velocity + self.ball_width < self.p1_y_pos + self.paddle_height + 10)):
             self.ball_x_velocity = -(self.ball_x_velocity - 0.1)
             self.ball_y_velocity = (self.p1_y_pos + self.paddle_height / 2 - self.ball_y_pos) / 16
             self.ball_y_velocity = -self.ball_y_velocity
 
-        elif (self.ball_x_pos + self.ball_x_velocity < 0):
-            self.p1_score = self.p1_score + 1
-            self.ball_x_pos = self.WIDTH / 2
-            self.ball_y_pos = self.HEIGHT / 2
-            self.ball_x_velocity = self.ball_x_normalspeed
-            self.ball_y_velocity = 0
-            self.p1_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
-            self.p2_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
-        if ((self.ball_x_pos + self.ball_x_velocity > self.p2_x_pos - self.paddle_width)
-            and (self.p2_y_pos < self.ball_y_pos + self.ball_y_velocity + self.ball_width and self.ball_y_pos + self.ball_y_velocity + self.ball_width < self.p2_y_pos + self.paddle_height + 10)):
+        elif self.ball_x_pos + self.ball_x_velocity < 0:
+            self.p1_score += 1
+            self.reset_ball_position()
+        if (self.ball_x_pos + self.ball_x_velocity > self.p2_x_pos - self.paddle_width
+            and (self.p2_y_pos < self.ball_y_pos + self.ball_y_velocity + self.ball_width < self.p2_y_pos + self.paddle_height + 10)):
             self.ball_x_velocity = -(self.ball_x_velocity + 0.1)
             self.ball_y_velocity = (self.p2_y_pos + self.paddle_height / 2 - self.ball_y_pos) / 16
             self.ball_y_velocity = -self.ball_y_velocity
-        elif (self.ball_x_pos + self.ball_x_velocity > self.HEIGHT):
-            self.p2_score = self.p2_score + 1
-            self.ball_x_pos = self.WIDTH / 2
-            self.ball_y_pos = self.HEIGHT / 2
-            self.ball_x_velocity = -self.ball_x_normalspeed
-            self.ball_y_velocity = 0
-            self.p1_y_pos = self.HEIGHT / 2 -self.paddle_height / 2
-            self.p2_y_pos = self.HEIGHT / 2 -self.paddle_height / 2
-        if (self.ball_y_pos + self.ball_y_velocity > self.HEIGHT or self.ball_y_pos + self.ball_y_velocity < 0):
+        elif self.ball_x_pos + self.ball_x_velocity > self.WIDTH:
+            self.p2_score += 1
+            self.reset_ball_position()
+        if self.ball_y_pos + self.ball_y_velocity > self.HEIGHT or self.ball_y_pos + self.ball_y_velocity < 0:
             self.ball_y_velocity = -self.ball_y_velocity
 
         self.ball_x_pos += self.ball_x_velocity
         self.ball_y_pos += self.ball_y_velocity
 
-        if (self.p1_score >= self.maxscore or self.p2_score >= self.maxscore):
+        if self.p1_score >= self.maxscore or self.p2_score >= self.maxscore:
             self.endgame()
+
+    def reset_ball_position(self):
+        self.ball_x_pos = self.WIDTH / 2
+        self.ball_y_pos = self.HEIGHT / 2
+        self.ball_x_velocity = self.ball_x_normalspeed
+        self.ball_y_velocity = 0
+        self.p1_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
+        self.p2_y_pos = self.HEIGHT / 2 - self.paddle_height / 2
 
     def run(self):
         self.is_running = True
@@ -109,206 +101,145 @@ class Game():
         thread.start()
 
     def endgame(self):
-        self.is_running = False #Stop la loop du jeu et sortira du thread
+        self.is_running = False
         self.has_finished = True
-
         print("Final scores: Player 1 =", self.p1_score, ", Player 2 =", self.p2_score)
-
+        # Suppose que la méthode Match.create_match_from_game et save() existent
         new_match = Match.create_match_from_game(self)
         new_match.save()
-        # games_online = [game for game in games_online if not game.has_finished]
+
+    def key_up_pressed(self, username):
+        if (username == self.player1):
+            self.p1_up = True
+        elif (username == self.player2):
+            self.p2_up = True
+
+    def key_up_released(self, username):
+        if (username == self.player1):
+            self.p1_up = False
+        elif (username == self.player2):
+            self.p2_up = False
+    
+    def key_down_pressed(self, username):
+        if (username == self.player1):
+            self.p1_down = True
+        elif (username == self.player2):
+            self.p2_down = True
+
+    def key_down_released(self, username):
+        if (username == self.player1):
+            self.p1_down = False
+        elif (username == self.player2):
+            self.p2_down = False
 
 
-class PongOnline(WebsocketConsumer):
+class BasePongConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.game = None
 
     def connect(self):
         self.accept()
-
-        user = self.scope['user']
-        for game in games_online:
-            if (game.has_finished == False and (game.player1 == user.username or game.player2 == user.username)):
-                self.game = game
-                break
-            if (self.game == None and game.has_finished == False and (game.player1 == "" or game.player2 == "")):
-                self.game = game
-                if (self.game.player1 == "" and self.game.player2 != user.username):
-                    self.game.player1 = user.username
-                elif (self.game.player2 == "" and self.game.player1 != user.username):
-                    self.game.player2 = user.username
-                break
-
-        if (self.game == None):
-            self.game = Game(2) # ICI on instancie un nouvel objet Game, qui à son tour lance sa methode start()
-            self.game.player1 = user.username
-            games_online.append(self.game)
-        if (self.game.player1 != "" and self.game.player2 != ""):
+        self.setup_game()
+        if self.game.player1 and self.game.player2:
             self.game.start()
+        self.send_connection_message()
+
+    def setup_game(self):
+        pass  # Doit être implémenté par les classes dérivées
+
+    def send_connection_message(self):
+        user = self.scope['user']
         self.send(text_data=json.dumps({
-            'type':'connection_established',
-            'message':'you are connected' + user.username,
-            'data':(self.game.__dict__)
+            'type': 'connection_established',
+            'message': 'you are connected ' + user.username,
+            'data': self.game.__dict__
         }))
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = self.scope['user'].username
-        if (message == 'update'):
+
+        if message == 'update':
             self.send_update()
-        if (message == 'key_up_pressed' and self.game.player1 == username):
-            self.game.p1_up = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 up'
-            }))
-        if (message == 'key_up_released' and self.game.player1 == username):
-            self.game.p1_up = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 up released'
-            }))
-        if (message == 'key_down_pressed' and self.game.player1 == username):
-            self.game.p1_down = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 down'
-            }))
-        if (message == 'key_down_released' and self.game.player1 == username):
-            self.game.p1_down = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 down released'
-            }))
-        if (message == 'p2key_up_pressed' and self.game.player2 == username):
+        elif 'pressed' in message or 'released' in message:
+            self.handle_key_event(message, username)
+
+    def handle_key_event(self, message, username):
+        if message == 'key_up_pressed':
+            self.game.key_up_pressed(username)
+        elif message == 'key_up_released':
+            self.game.key_up_released(username)
+        elif message == 'key_down_pressed':
+            self.game.key_down_pressed(username)
+        elif message == 'key_down_released':
+            self.game.key_down_released(username)
+        elif message == 'p2key_up_pressed' and self.game.game_type == "local":
             self.game.p2_up = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 up pressed'
-            }))
-        if (message == 'p2key_up_released' and self.game.player2 == username):
+        elif message == 'p2key_up_released' and self.game.game_type == "local":
             self.game.p2_up = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 up released'
-            }))
-        if (message == 'p2key_down_pressed' and self.game.player2 == username):
+        elif message == 'p2key_down_pressed' and self.game.game_type == "local":
             self.game.p2_down = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 down pressed'
-            }))
-        if (message == 'p2key_down_released' and self.game.player2 == username):
+        elif message == 'p2key_down_released' and self.game.game_type == "local":
             self.game.p2_down = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 down released'
-            }))
+        self.send_debug_message(message)
+
+    def send_debug_message(self, message):
+        self.send(text_data=json.dumps({
+            'type': 'debug',
+            'message': message
+        }))
 
     def send_update(self):
         self.send(text_data=json.dumps({
-            'type':'update received',
+            'type': 'update received',
             'data': self.game.__dict__
         }))
 
-# pourquoi _1 et _2, u lieu de demander un nom de joueur ? choisir l'un ou l'autre
-class PongLocal(WebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.game = None
 
-    def connect(self):
-        self.accept()
+class PongOnline(BasePongConsumer):
+    def setup_game(self):
+        user = self.scope['user']
+        for game in games_online:
+            if not game.has_finished and (game.player1 == user.username or game.player2 == user.username):
+                self.game = game
+                break
+            if not self.game and not game.has_finished and (not game.player1 or not game.player2):
+                self.game = game
+                if not self.game.player1 and game.player2 != user.username:
+                    self.game.player1 = user.username
+                elif not self.game.player2 and game.player1 != user.username:
+                    self.game.player2 = user.username
+                break
+        if not self.game:
+            self.game = Game(2, "online")
+            self.game.player1 = user.username
+            games_online.append(self.game)
 
+
+class PongLocal(BasePongConsumer):
+    def setup_game(self):
         user = self.scope['user']
         user1 = user.username
         user2 = user.username + "_2"
 
-        for game in games_local: #loop pour cherchr sil y a deja une partie en cours
-            if (game.has_finished == False and (game.player1 == user1 and game.player2 == user2)):
+        for game in games_local:
+            if not game.has_finished and game.player1 == user1 and game.player2 == user2:
                 self.game = game
                 break
-
-        if (self.game == None):
-            self.game = Game(5) # ICI on instancie un nouvel objet Game, qui à son tour lance sa methode start()
+        if not self.game:
+            self.game = Game(5, "local")
             self.game.player1 = user1
             self.game.player2 = user2
             games_local.append(self.game)
         else:
             self.game.is_running = True
-        if (self.game.player1 != "" and self.game.player2 != ""):
-            self.game.start()
-        self.send(text_data=json.dumps({
-            'type':'connection_established',
-            'message':'you are connected' + user1 + user2,
-            'data':(self.game.__dict__)
-        }))
-    def disconnect(self):
-        self.game.is_running = False
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        if (message == 'update'):
-            self.send_update()
-        elif message == 'setOpponentAlias':
-            opponent = text_data_json['opponent']
-            self.game.player2= opponent
-            # if (self.game.player1 != "" and self.game.player2 != ""):
-            #     self.game.start()
-        elif (message == 'key_up_pressed'):
-            self.game.p1_up = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 up'
-            }))
-        elif (message == 'key_up_released'):
-            self.game.p1_up = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 up released'
-            }))
-        elif (message == 'key_down_pressed'):
-            self.game.p1_down = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 down'
-            }))
-        elif (message == 'key_down_released'):
-            self.game.p1_down = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p1 down released'
-            }))
-        elif (message == 'p2key_up_pressed'):
-            self.game.p2_up = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 up pressed'
-            }))
-        elif (message == 'p2key_up_released'):
-            self.game.p2_up = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 up released'
-            }))
-        elif (message == 'p2key_down_pressed'):
-            self.game.p2_down = True
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 down pressed'
-            }))
-        elif (message == 'p2key_down_released'):
-            self.game.p2_down = False
-            self.send(text_data=json.dumps({
-                'type':'debug',
-                'message':'p2 down released'
-            }))
-
-    def send_update(self):
-        self.send(text_data=json.dumps({
-            'type':'update received',
-            'data': self.game.__dict__
-        }))
+        if message == 'setOpponentAlias':
+            self.game.player2 = text_data_json['opponent']
+        else:
+            super().receive(text_data)
