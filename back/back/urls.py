15ed23a8548a312836ce99686629054ccb003aa2
jlupito <jl.arthaud@protonmail.com>
2024-05-23 16:55:45 +0200

    post merge

--- back/back/urls.py
+++ back/back/urls.py
@@ -14,6 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('', include('pong.urls')),
+#     path('admin/', admin.site.urls),
+# ]
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
