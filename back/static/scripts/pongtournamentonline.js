f234513bb80131c6ca21fa36029a86894e9da741
jlupito <jl.arthaud@protonmail.com>
2024-05-23 16:31:21 +0200

    pre merge

--- back/static/scripts/pongtournamentonline.js
+++ back/static/scripts/pongtournamentonline.js
@@ -90,7 +90,7 @@ function runsocket(){
     let player1 = ""
     let player2 = ""
     
-    const canvas = document.getElementById('CanvasOnline');
+    const canvas = document.getElementById('CanvasTourOnline');
     
     const ctx = canvas.getContext('2d');
     
