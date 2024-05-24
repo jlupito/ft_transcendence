d2b97127dfef7f5ee8a25524e5f7fdd5cfac0e56
jlupito <jl.arthaud@protonmail.com>
2024-05-24 18:11:45 +0200

    some shit

--- back/pong/static/scripts/script.js
+++ back/pong/static/scripts/script.js
@@ -131,44 +131,134 @@ document.addEventListener('DOMContentLoaded', function() {
 //         // Afficher une erreur si quelque chose se passe mal
 //         console.error('There has been a problem with your fetch operation:', error);
 //     });
-// document.addEventListener('DOMContentLoaded', function() {
-//   function setupLocalMatchModal() {
-//     document.querySelector('#localMatchForm').addEventListener('submit', function(event) {
-//       event.preventDefault();
-//       startLocalGame();
-//       });
-//       function startLocalGame() {
 
-//       document.querySelector('#form-view').style.display = 'none';
-//       document.querySelector('#game-view').style.display = 'block';
-//       var pongScript = document.createElement('script');
-//       pongScript.src = '../static/scripts/ponglocal.js'; // Chemin vers pong.js
-//       pongScript.defer = true;
-//       document.body.appendChild(pongScript);
-//       }
-//   }
-//   setupLocalMatchModal();
+// script pour le chronometre
 
-//   function setupMatchModal() {
-//     //   waitingMessage(); // Afficher le message d'attente au début avant que les deux joueurs soient en Mode Match
-//       // Mettre ici logique pour vérifier si les joueurs sont prêts à commencer le jeu
-//       startGame() //pour lancer le jeu
+var width = 400,
+height = 400,
+timePassed = 0,
+timeLimit = 30;
 
-//     //   setTimeout(startGame, 6000); // a supprimer une fois le declanchement des connexions faites
-//   }
+var fields = [{
+value: timeLimit,
+size: timeLimit,
+update: function() {
+return timePassed = timePassed + 1;
+}
+}];
 
-//   function waitingMessage() { // on fait un rappel du html pour le message d'attente
-//       const modalBody = document.querySelector('#onlineMatchModal .modal-body');
-//       modalBody.innerHTML = '<p class="my-auto me-2">Waiting for an opponent</p>' +
-//                              '<div class="dot"></div>' +
-//                              '<div class="dot"></div>' +
-//                              '<div class="dot"></div>';
-//   }
+var nilArc = d3.svg.arc()
+.innerRadius(width / 3 - 133)
+.outerRadius(width / 3 - 133)
+.startAngle(0)
+.endAngle(2 * Math.PI);
 
-//   function startGame() {
-//       const modalBody = document.querySelector('#onlineMatchModal .modal-body');
-//       modalBody.innerHTML = '<div class="canvas" width="600" height="600"><canvas id="CanvasOnline" width="600" height="600"></canvas></div>';
-//   }
+var arc = d3.svg.arc()
+.innerRadius(width / 3 - 55)
+.outerRadius(width / 3 - 25)
+.startAngle(0)
+.endAngle(function(d) {
+return ((d.value / d.size) * 2 * Math.PI);
+});
 
-//   setupMatchModal();
-// });
+var svg = d3.select(".countdown").append("svg")
+.attr("width", width)
+.attr("height", height);
+
+var field = svg.selectAll(".field")
+.data(fields)
+.enter().append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+.attr("class", "field");
+
+var back = field.append("path")
+.attr("class", "path path--background")
+.attr("d", arc);
+
+var path = field.append("path")
+.attr("class", "path path--foreground");
+
+var label = field.append("text")
+.attr("class", "label")
+.attr("dy", ".35em");
+
+(function update() {
+
+field
+.each(function(d) {
+  d.previous = d.value, d.value = d.update(timePassed);
+});
+
+path.transition()
+.ease("elastic")
+.duration(500)
+.attrTween("d", arcTween);
+
+if ((timeLimit - timePassed) <= 10)
+pulseText();
+else
+label
+.text(function(d) {
+  return d.size - d.value;
+});
+
+if (timePassed <= timeLimit)
+setTimeout(update, 1000 - (timePassed % 1000));
+else
+destroyTimer();
+
+})();
+
+function pulseText() {
+back.classed("pulse", true);
+label.classed("pulse", true);
+
+if ((timeLimit - timePassed) >= 0) {
+label.style("font-size", "120px")
+  .attr("transform", "translate(0," + +4 + ")")
+  .text(function(d) {
+    return d.size - d.value;
+  });
+}
+
+label.transition()
+.ease("elastic")
+.duration(900)
+.style("font-size", "90px")
+.attr("transform", "translate(0," + -10 + ")");
+}
+
+function destroyTimer() {
+label.transition()
+.ease("back")
+.duration(700)
+.style("opacity", "0")
+.style("font-size", "5")
+.attr("transform", "translate(0," + -40 + ")")
+.each("end", function() {
+  field.selectAll("text").remove()
+});
+
+path.transition()
+.ease("back")
+.duration(700)
+.attr("d", nilArc);
+
+back.transition()
+.ease("back")
+.duration(700)
+.attr("d", nilArc)
+.each("end", function() {
+  field.selectAll("path").remove()
+});
+}
+
+function arcTween(b) {
+var i = d3.interpolate({
+value: b.previous
+}, b);
+return function(t) {
+return arc(i(t));
+};
+}
+
