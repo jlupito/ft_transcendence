ea53c7aa67a677b7a28e5cbf5ac66ee1161dbcc4
sachayakovleff <sacha.yakovleff@outlook.fr>
2024-05-27 16:59:12 +0200

    daphne

--- back/back/asgi.py
+++ back/back/asgi.py
@@ -7,26 +7,32 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+from .wsgi import *  # add this line to top of your code
+import os, django
 from django.urls import re_path
 from pong import consumers
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import pong.routing
 
+# DockerDjangoNginx is my project name
+# your routing.py file should be in this location where the wsgi.py file is placed
+# DockerDjangoNginx/DockerDjangoNginx/routing.py
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
+    'https': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
         URLRouter(
-            # pong.routing.websocket_urlpatterns
             [
             re_path(r'ws/socket-pong-local/', consumers.PongLocal.as_asgi()),
             re_path(r'ws/socket-pong-online/', consumers.PongOnline.as_asgi()),
             re_path(r'ws/socket-pong-tournament-online/', consumers.PongOnlineTournament.as_asgi())
             ]
         )
-    )
+    ),
 })
