d008445b215f671fe81ff43f0c89b6b8ac6db99b
jlupito <jl.arthaud@protonmail.com>
2024-06-24 12:13:10 +0200

    status indicator fontional

--- back/pong/static/scripts/status_online.js
+++ back/pong/static/scripts/status_online.js
@@ -19,9 +19,11 @@ function runsocketStatus() {
 
                 console.log('Status Indicator Element was found for user', data.user_id);
                 if (data.status == 'is_online') {
+                    statusIndicator.classList.remove('border', 'border-2', 'border-danger'); 
                     statusIndicator.classList.add('border', 'border-2', 'border-success'); // bordure verte
                 }
                 else if (data.status == 'is_playing') {
+                    statusIndicator.classList.remove('border', 'border-2', 'border-success'); 
                     statusIndicator.classList.add('border', 'border-2', 'border-danger'); // bordure bleue
                 }
                 else if (data.status == 'is_offline') {
